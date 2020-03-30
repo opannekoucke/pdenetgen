79de07e940a7ba0e706ed5be1f140a4286746125
Olivier Pannekoucke <o.pannekoucke@gmail.com>
2020-03-30 12:41:18 +0200

    refactor : rm print from derivative

--- pdenetgen/symbolic/nn_builder.py
+++ pdenetgen/symbolic/nn_builder.py
@@ -690,9 +690,7 @@ class DerivativeFactory(object):
         dimension = len(kernel_size)
 
         if periodic:
-            print('Create a periodic boundary')
             def PeriodicDerivative(conv):
-                print('New PDE-NetGen version')
                 periodized = PeriodicFactory(kernel_size)(conv)
                 derivative = DerivativeFactory(kernel_size, kernel, name, periodic=False, nfilter=nfilter)(periodized)
                 cropped = CropFactory(kernel_size)(derivative)
