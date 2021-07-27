49b9cd31617d41436fb317a88005f8c2f1d8c39d
Olivier Pannekoucke <o.pannekoucke@gmail.com>
2021-07-27 10:43:05 +0200

    feat : add theoretical consistance value

--- pdenetgen/symbolic/test/test_consistancy.py
+++ pdenetgen/symbolic/test/test_consistancy.py
@@ -1,16 +1,16 @@
 from unittest import TestCase
-from pdenetgen import remove_eval_derivative
+from pdenetgen.symbolic import remove_eval_derivative
 from pdenetgen.symbolic.finite_difference import finite_difference
 from sympy import Function, Derivative, symbols
-
-
 class TestFiniteDifferenceConsistancy(TestCase):
     """
-    Test of the consistancy of finite difference operator
+    Test of the **second-order consistancy** of finite difference operator
 
     """
 
-    def test_even_order_derivative_consistancy(self):    
+    consistancy_order = 2 # theoretical consistancy order for the package.
+
+    def test_even_order_derivative_consistancy(self):            
         # Set function
         x, dx = symbols('x dx')
         u = Function('u')(x)
@@ -22,12 +22,12 @@ class TestFiniteDifferenceConsistancy(TestCase):
         fd_expr = finite_difference(expr)
 
         # Validation of the consistancy
-        consistance_fd_expr = fd_expr.series(dx,0,2)
+        consistance_fd_expr = fd_expr.series(dx,0, self.consistancy_order)
         consistance_fd_expr = consistance_fd_expr.removeO()
         consistance_fd_expr = remove_eval_derivative(consistance_fd_expr)
         self.assertEqual(consistance_fd_expr, expr)        
 
-    def test_odd_order_derivative_consistancy(self):
+    def test_odd_order_derivative_consistancy(self):        
         # Set function
         x, dx = symbols('x dx')
         u = Function('u')(x)
@@ -39,8 +39,12 @@ class TestFiniteDifferenceConsistancy(TestCase):
         fd_expr = finite_difference(expr)
 
         # Validation of the consistancy
-        consistance_fd_expr = fd_expr.series(dx,0,2)
+        consistance_fd_expr = fd_expr.series(dx,0, self.consistancy_order)
         consistance_fd_expr = consistance_fd_expr.removeO()
         consistance_fd_expr = remove_eval_derivative(consistance_fd_expr)
-        self.assertEqual(consistance_fd_expr, expr)        
+        self.assertEqual(consistance_fd_expr, expr)
 
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
